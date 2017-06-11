d95e595475ce4bc4657eadb02720cf0f154351ac
ringneck <norman.southcastle@gmail.com>
2017-06-11 21:49:57 +0900

    Create naver-tts-v1.php
    convert from mp3 to wav 8Khz, mono

---
+++ naver-tts-v1.php
@@ -0,0 +1,32 @@
+<?php
+  $client_id = "YOUR_CLIENT_ID";
+  $client_secret = "YOUR_CLIENT_SECRET";
+  $encText = urlencode("반갑습니다.");
+  $postvars = "speaker=mijin&speed=0&text=".$encText;
+  $url = "https://openapi.naver.com/v1/voice/tts.bin";
+  $is_post = true;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_POST, $is_post);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $postvars);
+  $headers = array();
+  $headers[] = "X-Naver-Client-Id: ".$client_id;
+  $headers[] = "X-Naver-Client-Secret: ".$client_secret;
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  $response = curl_exec ($ch);
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  echo "status_code:".$status_code."<br>";
+  curl_close ($ch);
+  if($status_code == 200) {
+    //echo $response;
+    // convert from mp3 to wav 8Khz, mono
+    shell_exec('sox tts.mp3 -c1 -r8000  tts.wav');
+    $fp = fopen("tts.wav", "w+");
+    fwrite($fp, $response);
+    fclose($fp);
+    echo "<a href='tts.wav'>TTS재생</a>";
+  } else {
+    echo "Error 내용:".$response;
+  }
+?>
